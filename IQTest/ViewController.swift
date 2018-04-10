7bd73a56ed15ddb7b79d28b228a9625542485fc7
Jerry Shi <jerry.shi@javoft.com>
2018-04-09 20:01:07 -0400

    Initial Commit

---
+++ IQTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IQTest
+//
+//  Created by Jerry Shi on 2018-04-09.
+//  Copyright © 2018 Javoft Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
